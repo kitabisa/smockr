73092e69c08548cbaec4aa2abd1536a81c40071e
Ade Hery Shopyan <adeheryshopyan@gmail.com>
2024-03-19 16:01:28 +0700

    fix: cors issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,30 @@ const nextConfig = {
   output: 'standalone',
   swcMinify: true,
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+            // value: process.env.ALLOWED_METHOD,
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "*",
+            // value: process.env.ALLOWED_HEADERS,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

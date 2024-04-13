94b14466205ef28b13b18a90001848e5084703f3
Ade Hery Shopyan <51158020+adeherysh@users.noreply.github.com>
2024-04-13 22:29:34 +0700

    refactor: remove unused utils

--- utils/parsers/parseBoolean.ts
+++
@@ -1,5 +0,0 @@
-import type { JSONSchema7 } from 'json-schema'
-import { z } from 'zod'
-
-export const parseBoolean = (_schema: JSONSchema7 & { type: 'boolean' }) =>
-  z.boolean()
--- utils/parsers/utils.ts
+++
@@ -1,2 +0,0 @@
-export const withRecursive = (z: any, withFunctions: ((z: any) => any)[]) =>
-  withFunctions.reduce((z, fn) => fn(z), z)
